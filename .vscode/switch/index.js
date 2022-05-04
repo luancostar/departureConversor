2bf625cf9e8fb19cf896c7124f810153f3a19110
luancostar <luancostar@gmail.com>
2022-05-04 18:51:00 -0300

    Primeiro commit

---
+++ .vscode/switch/index.js
@@ -0,0 +1,6 @@
+
+//* Esse código serve para converter distâncias espaciais*//
+let yearsLightDistance = prompt("Qual a distância em anos luz ? ")
+
+let chosenOption = prompt("Em que medida gostaria de converter a distância ?\n1. Parsec\n 2.Unidade astronômica \n3. Quilômetros")
+
